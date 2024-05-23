c8c6c27c02899a39b319971600ef0d0111c96d70
Adrian Düsselberg <115228849+DuesselbergAdrian@users.noreply.github.com>
2024-05-23 10:09:06 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrianduesselberg/root-graphic-tests
-# Build directory: /home/adrianduesselberg/root-graphic-tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("googletest")
--- build/test/CMakeFiles/JsonTests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for JsonTests.
-# This may be replaced when dependencies are built.
