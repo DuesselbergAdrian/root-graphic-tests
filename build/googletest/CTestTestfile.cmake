557ee0d183a91dabc8bf473d77dfaa8174cda787
Adrian Duesselberg <a.duesselberg@gmail.com>
2024-06-04 15:36:16 +0200

    Update gitignore

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrianduesselberg/root-graphic-tests/googletest
-# Build directory: /home/adrianduesselberg/root-graphic-tests/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
