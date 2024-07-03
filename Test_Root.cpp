1f42827c428041c26086be16ceb7522af646cb71
Adrian Duesselberg <a.duesselberg@gmail.com>
2024-07-03 16:35:51 +0200

    Updates Root tests

--- Test_Root.cpp
+++ Test_Root.cpp
@@ -10,12 +10,11 @@
 #include "TString.h"
 #include "TApplication.h"
 #include "TWebCanvas.h"
-#include "TString.h"
 #include "TLatex.h"
 #include <TSystem.h>
 
 #include <string>
-#include <fstream> 
+#include <fstream>
 #include <sstream>
 #include <iostream>
 #include <algorithm>
@@ -25,23 +24,61 @@
 #include <stdexcept>
 #include <filesystem>
 
-//FUNCTIONS
+// FUNCTION DECLARATIONS
+
+// JSON Preprocessing
+std::string preprocessJSONContent(const std::string& jsonString);
+
+// JSON Comparison
+bool compare_json(const TString& jsonOutput, const std::string& ref_filename, const std::string& macroName);
+
+// JSON Test
+void test_json(TCanvas* c1, const std::string& macroName);
+
+// File Content Reading
+std::string readFileToString(const std::string& filePath);
+
+// SVG Preprocessing
+std::string preprocessSVGContent(const std::string& svgContent);
+
+// SVG Comparison
+bool compareSVGFiles(const std::string& filePath1, const std::string& filePath2);
+
+// SVG Test
+void test_svg(TCanvas* c1, const std::string& macroName);
+
+// PDF Preprocessing
+std::string preprocessPDFContent(const std::string& pdfString);
+
+// PDF Comparison
+bool comparePDFFiles(const std::string& filePath1, const std::string& filePath2);
+
+// PDF Test
+void test_pdf(TCanvas* c1, const std::string& macroName);
+
+// Root Test
+void Test_Root(const std::string& macroName, const std::string& test_type, const std::string& macro_folder);
+
+// MAIN FUNCTION
+int main(int argc, char** argv);
+
+// FUNCTION DEFINITIONS
 
 //---------------------JSON----------------------------------------------------------------
 std::string preprocessJSONContent(const std::string& jsonString) {
     std::string result = jsonString;
     // Remove fTsumwx key
-    std::regex fTsumwxRegex(R"(^"fTsumwx" : \d+\.\d+,$)");
+    std::regex fTsumwxRegex(R"("fTsumwx" : \d+\.\d+,)");
     result = std::regex_replace(result, fTsumwxRegex, "");
 
     // Remove fTsumwx2 key
-    std::regex fTsumwx2Regex(R"(^"fTsumwx2" : \d+\.\d+,$)");
+    std::regex fTsumwx2Regex(R"("fTsumwx2" : \d+\.\d+,)");
     result = std::regex_replace(result, fTsumwx2Regex, "");
     return result;
 }
 
-bool compare_json(const TString& jsonOutput, const std::string& ref_filename, const std::string& macroName){
-   // Read the reference JSON content from file
+bool compare_json(const TString& jsonOutput, const std::string& ref_filename, const std::string& macroName) {
+    // Read the reference JSON content from file
     std::ifstream refFile(ref_filename);
     if (!refFile.is_open()) {
         std::string jsonFilePath = "./json_ref/" + macroName + ".json";
@@ -62,22 +99,19 @@ bool compare_json(const TString& jsonOutput, const std::string& ref_filename, co
     // Remove specific lines from both JSON strings
     std::string produced_json = preprocessJSONContent(jsonOutput.Data());
     std::string reference_json = preprocessJSONContent(refBuffer.str());
-    
-    //std::cout << produced_json << std::endl; 
 
     // Compare the created JSON to the reference JSON
     std::cerr << "Length of produced JSON: " << jsonOutput.Length() << std::endl;
     std::cerr << "Length of reference JSON: " << refBuffer.str().length() << std::endl;
 
-    //return jsonOutput.Data() == refBuffer.str();
     return produced_json == reference_json;
 }
 
-void test_json(TCanvas* c1, const std::string& macroName){
-     // Create JSON output from canvas
+void test_json(TCanvas* c1, const std::string& macroName) {
+    // Create JSON output from canvas
     TString jsonOutput = TWebCanvas::CreateCanvasJSON(c1, 1, kFALSE);
 
-    // Path to the reference and prodouced JSON file
+    // Path to the reference and produced JSON file
     std::string ref_filename = "./json_ref/" + macroName + ".json";
     std::string jsonFilePath = "./json_pro/" + macroName + "_pro.json";
 
@@ -99,6 +133,7 @@ void test_json(TCanvas* c1, const std::string& macroName){
         exit(EXIT_FAILURE);
     }
 }
+
 //---------------------SVG/PDF-------------------------------------------------------------
 // Function to read file content into a string
 std::string readFileToString(const std::string& filePath) {
@@ -146,84 +181,94 @@ bool compareSVGFiles(const std::string& filePath1, const std::string& filePath2)
     }
 }
 
-void test_svg(TCanvas* c1, const std::string& macroName){
+void test_svg(TCanvas* c1, const std::string& macroName) {
     // Paths to the reference and generated SVG files
     std::string refFilePath = "./old_svg_ref/" + macroName + ".svg";
-     std::string tempFilePath = "./old_svg_pro/temp_" + macroName + "_pro.svg";
+    std::string tempFilePath = "./old_svg_pro/temp_" + macroName + "_pro.svg";
     std::string genFilePath = "./old_svg_pro/" + macroName + "_pro.svg";
 
     // Check if the reference file exists
-    if(!std::filesystem::exists(refFilePath)){
+    if (!std::filesystem::exists(refFilePath)) {
         std::cout << "Reference file not found. Saving generated file as reference: " << macroName << std::endl;
         c1->SaveAs(refFilePath.c_str());
         exit(EXIT_FAILURE);
     } else {
         // Save the generated SVG file
         c1->SaveAs(tempFilePath.c_str());
-}
-        // Compare the generated SVG file with the reference SVG file
-        if (compareSVGFiles(refFilePath, genFilePath)) {
-            std::cout << "SVG test passed for "<< macroName << std::endl;
-            std::filesystem::remove(tempFilePath);
-        } else {
-            std::cout << "SVG test failed for "<< macroName << std::endl;
-            std::filesystem::rename(tempFilePath, genFilePath);
-            exit(EXIT_FAILURE);
+    }
+
+    // Compare the generated SVG file with the reference SVG file
+    if (compareSVGFiles(refFilePath, tempFilePath)) {
+        std::cout << "SVG test passed for " << macroName << std::endl;
+        std::filesystem::remove(tempFilePath);
+    } else {
+        std::cout << "SVG test failed for " << macroName << std::endl;
+        std::filesystem::rename(tempFilePath, genFilePath);
+        exit(EXIT_FAILURE);
     }
 }
 
 //---------------------PDF-----------------------------------------------------------------
 std::string preprocessPDFContent(const std::string& pdfString) {
     std::string result = pdfString;
-    // Remove
+    // Regular expressions to match the metadata patterns
+    std::regex creationDatePattern(R"(/CreationDate \(D:\d{14}.*?\))");
+    std::regex modDatePattern(R"(/ModDate \(D:\d{14}[-+Z].*?\))");
+    std::regex titlePattern(R"(/Title \(.*?\))");
 
+    // Remove the matched patterns from the content
+    result = std::regex_replace(result, creationDatePattern, "");
+    result = std::regex_replace(result, modDatePattern, "");
+    result = std::regex_replace(result, titlePattern, "");
     return result;
 }
+
 // Function to compare two PDF files (old graphics)
 bool comparePDFFiles(const std::string& filePath1, const std::string& filePath2) {
     try {
         std::string content1 = readFileToString(filePath1);
         std::string content2 = readFileToString(filePath2);
+        //std::cout << content1 << std::endl;
 
         content1 = preprocessPDFContent(content1);
         content2 = preprocessPDFContent(content2);
 
         return content1 == content2;
-
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return false;
     }
 }
 
-void test_pdf(TCanvas* c1, const std::string& macroName){
+void test_pdf(TCanvas* c1, const std::string& macroName) {
     // Paths to the reference and generated SVG files
     std::string refFilePath = "./pdf_ref/" + macroName + ".pdf";
     std::string tempFilePath = "./pdf_pro/temp_" + macroName + "_pro.pdf";
     std::string genFilePath = "./pdf_pro/" + macroName + "_pro.pdf";
 
     // Check if the reference file exists
-    if(!std::filesystem::exists(refFilePath)){
+    if (!std::filesystem::exists(refFilePath)) {
         std::cout << "Reference file not found. Saving generated file as reference: " << macroName << std::endl;
         c1->SaveAs(refFilePath.c_str());
         exit(EXIT_FAILURE);
     } else {
         // Save the generated PDF file
         c1->SaveAs(tempFilePath.c_str());
-}
-        // Compare the generated SVG file with the reference SVG file
-        if (comparePDFFiles(refFilePath, genFilePath)) {
-            std::cout << "PDF test passed for "<< macroName << std::endl;
-            std::filesystem::remove(tempFilePath);
-        } else {
-            std::cout << "PDF test failed for "<< macroName << std::endl;
-            std::filesystem::rename(tempFilePath, genFilePath);
-            exit(EXIT_FAILURE);
+    }
+
+    // Compare the generated PDF file with the reference PDF file
+    if (comparePDFFiles(refFilePath, tempFilePath)) {
+        std::cout << "PDF test passed for " << macroName << std::endl;
+        std::filesystem::remove(tempFilePath);
+    } else {
+        std::cout << "PDF test failed for " << macroName << std::endl;
+        std::filesystem::rename(tempFilePath, genFilePath);
+        exit(EXIT_FAILURE);
     }
 }
 
 // TEST ROOT MACRO -------------------------------------------------------------------------------
-void Test_Root(const std::string& macroName, const std::string& test_type, const std::string& macro_folder){
+void Test_Root(const std::string& macroName, const std::string& test_type, const std::string& macro_folder) {
     // Set paths
     std::string macroPath = macro_folder + "/" + macroName + ".C";
     gROOT->SetMacroPath("./macros");
@@ -245,20 +290,19 @@ void Test_Root(const std::string& macroName, const std::string& test_type, const
     // o === test the SVG creation in ROOT (old graphics with --web=off)
     // p == test the PDF creation in ROOT (old graphics with --web=off)
 
-    if(test_type == "j"){
-        test_json(c1,macroName);
+    if (test_type == "j") {
+        test_json(c1, macroName);
     }
-    if(test_type == "o"){
-        test_svg(c1,macroName);
+    if (test_type == "o") {
+        test_svg(c1, macroName);
     }
-    if(test_type == "p"){
-        test_pdf(c1,macroName);
+    if (test_type == "p") {
+        test_pdf(c1, macroName);
     }
     return;
 }
 
-
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     if (argc != 4) {
         std::cerr << "Usage: " << argv[0] << " <macro_name> <test_type> <macro_folder>" << std::endl;
         return 1;
@@ -267,6 +311,6 @@ int main(int argc, char **argv) {
     std::string macroName = argv[1];
     std::string test_type = argv[2];
     std::string macro_folder = argv[3];
-    Test_Root(macroName,test_type, macro_folder);
+    Test_Root(macroName, test_type, macro_folder);
     return EXIT_SUCCESS;
 }
